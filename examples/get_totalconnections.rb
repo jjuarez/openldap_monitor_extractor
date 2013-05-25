19543c2711ccca7573d8b9d1b654167717b3ef2a
Javier Juarez <javier.juarez@gmail.com>
2013-05-25 17:40:07 +0200

    New name for the example

--- examples/get_totalconnections.rb
+++ examples/get_totalconnections.rb
@@ -2,13 +2,13 @@
 
 $:.unshift File.join(File.dirname(__FILE__), %w{.. lib})
 
+
 require 'rubygems'
 require 'yaml'
 require 'openldap_monitor_extractor'
 
 
-config_file = File.join(File.dirname(__FILE__), %w{.. config ldap_sample_settings.yml})
-config      = YAML.load_file(config_file)
+config = YAML.load_file(ARGV[0])
 
 OpenldapMonitorExtractor.configure(config)
 puts OpenldapMonitorExtractor.get(:total_connections)
