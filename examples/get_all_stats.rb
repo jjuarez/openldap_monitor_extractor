aa5f424d046dca7f42b174e3b905c90ca70c6b76
Javier Juarez <javier.juarez@gmail.com>
2013-05-29 12:07:35 +0200

    Prety print

---
+++ examples/get_all_stats.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+$:.unshift File.join(File.dirname(__FILE__), %w{.. lib})
+
+
+require 'rubygems'
+require 'yaml'
+require 'openldap_monitor_extractor'
+
+
+if ARGV[0] && File.exist?(ARGV[0])
+  
+  config = YAML.load_file(ARGV[0])
+  puts OpenldapMonitorExtractor.configure(config).get(:all).inspect
+else
+  
+  puts "usage: #{$0} config_file.yml"
+end
